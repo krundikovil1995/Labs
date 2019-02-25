8f56db1f41ad3c9551f6954a9193d7f7a326f1ed
krundikovil1995 <20krundikovil1995@mail.ru>
2019-02-25 12:10:59 +0300

    made some changes in the 6 task

--- Лаба 4/1/6.php
+++ Лаба 4/1/6.php
@@ -1,6 +1,6 @@
 <?php
 
-$date = "In this task we are reform date to a new view! 29/11/2018, 29.11.2016, 1.2.16, 2/11/12, 2/12/2015 ";
+$date = "In this task we are reform date to a new view! 29/11/2018, 29.11.2016, 1.2.16, 2/11/95, 2/12/2015 ";
 
 
 function convertDate ($x){
@@ -9,7 +9,11 @@ function convertDate ($x){
     if (preg_match('/[\d]{1,2}[-][\d]{1,2}[-][\d]{2}$/', $date)){
         $length = strlen($date);
         $numb = substr($date, -2);
-        $date = substr_replace($date, "20$numb", -2);
+        if ($numb<20) {
+            $date = substr_replace($date, "20$numb", -2);
+        } else {
+            $date = substr_replace($date, "19$numb", -2);
+        }
     }
     return '<b style="background-color: red">'. (date('d-m-Y', (strtotime("+1 years", strtotime($date))))).'</b>';
 }
