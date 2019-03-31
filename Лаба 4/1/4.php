4b2a45fd709d7542e95d3439aeaed8e785557bc8
krundikovil1995 <20krundikovil1995@mail.ru>
2019-03-31 14:56:42 +0300

    corrected 4 task!

--- Лаба 4/1/4.php
+++ Лаба 4/1/4.php
@@ -5,7 +5,7 @@ $str = 'this is the URL of the site: https://news.tut.by/economics/627365.html';
 function changeStyleURL ($t){
     $url = trim($t[0], '.');
 
-    return '<b style="background-color: red"><a href="URL">'.$url.'</a></b>';
+    return '<b style="background-color: red"><a href="'.$url.'">'.$url.'</a></b>';
 }
 
 echo preg_replace_callback('/(http|https|ftp)(:\/\/)(?:([\w!-$^]+)[.][\w]+)([\w\/?=+-_а-яА-ЯёЁ&%]*[\w\/+=?-а-яА-ЯёЁ:&%])?/', 'changeStyleURL', $str);
